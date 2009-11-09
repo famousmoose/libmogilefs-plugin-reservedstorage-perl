74bd097cefcf36f798fd44db8ae5552d44f85bb5
stuart <stuart@459b5371-7e0d-0410-90b4-cdf39e697ed8>
2009-11-09 16:51:02 +0000

    [svn-inject] Forking libmogilefs-plugin-reservedstorage-perl source to Trunk
    git-svn-id: https://svn.mediagraft.com/svn/sysadmin/trunk/debian/mogilefs/libmogilefs-plugin-reservedstorage-perl/trunk@20448 459b5371-7e0d-0410-90b4-cdf39e697ed8

---
+++ lib/MogileFS/Plugin/ReservedStorage.pm
@@ -0,0 +1,73 @@
+# ReservedStorage plugin for MogileFS
+
+package MogileFS::Plugin::ReservedStorage;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+$VERSION = eval $VERSION;
+
+use MogileFS::Server;
+use MogileFS::Util;
+
+sub conv_to_mb($) {
+    my ($val) = @_;
+    my ($n, $u) = ($val =~ /^(\d+)\s*([kMGT])[bB]?$/) or return undef;
+    $n /= 1024 if $n eq 'k';
+    $n *= 1024 if $n eq 'G' || $n eq 'T';
+    $n *= 1024 if $n eq 'T';
+    return $n;
+}
+
+
+sub sort_devs_by_freespace {
+    my @devices_with_weights;
+
+    my $sto = Mgd::get_store();
+    my $default_reserved = conv_to_mb $sto->server_setting('reserved_storage');
+
+    foreach my $dev (@_) {
+        next unless $dev->exists;
+
+        my $mb_resrv = conv_to_mb 
+            $sto->server_setting('reserved_storage_dev_'.$dev->devid);
+        $mb_resrv = $default_reserved if not defined $mb_resrv;
+        $mb_resrv = 0 if not defined $mb_resrv;
+
+        next if $dev->mb_free < $mb_resrv;
+        
+        my $percent = ($dev->mb_free - $mb_resrv) / $dev->mb_free 
+           * $dev->precent_free;
+
+        my $weight = 100 * $percent;
+        push @devices_with_weights, [$dev, $weight];
+    }
+
+    my @list = MogileFS::Util::weighted_list(@devices_with_weights);
+
+    return @list;
+}
+
+sub load {
+    MogileFS::register_global_hook( 'cmd_create_open_order_devices', sub {
+        my $devices = shift;
+        my $sorted_devs = shift;
+
+        @$sorted_devs = MogileFS::Worker::Query::sort_devs_by_freespace(@$devices);
+
+        return 1;
+    });
+
+    return 1;
+}
+
+sub unload {
+    # remove our hooks
+    MogileFS::unregister_global_hook( 'cmd_create_open_order_devices' );
+
+    return 1;
+}
+
+
+1;
